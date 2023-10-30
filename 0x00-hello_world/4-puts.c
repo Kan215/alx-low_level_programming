08197ed3c5ce86255a8fb3648950008643b4c7a0
Kan215 <katamialvin215@gmail.com>
2023-10-30 07:34:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main . A c programm that prints a line using puts fumction
- * Return 0: (program execution successful)
+ * main - A c programm that prints a line using puts fumction
+ * Return: (program execution successful 0)
  */
 int main(void)
 {
-puts('\"Programming is like building a multilingual puzzle")
+puts('\"Programming is like building a multilingual puzzle");
 return (0);
 }
