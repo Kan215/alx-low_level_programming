e18ee0cfb49de2aee151ea21754cb075d79f4a0f
Kan215 <katamialvin215@gmail.com>
2023-10-31 06:30:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - Start execution
+* Description: prints the alphabet, in lowercase
+* Return: 0(Success)
+*/
+void print_alphabet(void)
+{
+char  alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+return (0);
+}
