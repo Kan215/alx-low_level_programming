e16f7ee2fcf85bbf2ebf456eeca22fbf1582d090
Kan215 <katamialvin215@gmail.com>
2023-11-01 11:58:24 -0700

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+    if (n <= 98) {
+        for (int i = n; i <= 98; i++) {
+            if (i != 98) {
+                printf("%d, ", i);
+            } else {
+                printf("%d\n", i);
+            }
+        }
+    } else {
+        for (int i = n; i >= 98; i--) {
+            if (i != 98) {
+                printf("%d, ", i);
+            } else {
+                printf("%d\n", i);
+            }
+        }
+    }
+}
+
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * _abs - computes the value of an absolute integer
- * Return: Absolute value of zero
- */
+* _abs - computes the value of an absolute i.
+* c: The number to be computed.
+* Reurn: Absolute value of the number or zero.
+*/
 int _abs(int c)
 {
-	if (c<0)
-	{
-	int abs-val;
-	abs_val = c * -1;
-	return (abs_val);
-	}
-	return (c);
+if (c<0)
+{
+int abs_val;
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
 }
