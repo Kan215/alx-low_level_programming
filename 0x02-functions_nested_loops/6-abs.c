eebc85b13ce08029c627964d0dda6227a4359479
Kan215 <katamialvin215@gmail.com>
2023-10-31 19:06:34 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the value of an absolute integer
+ * Return: Absolute value of zero
+ */
+int _abs(int c)
+{
+	if (c<0)
+	{
+	int abs-val;
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
