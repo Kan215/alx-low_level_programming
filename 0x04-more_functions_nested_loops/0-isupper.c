5ba0ab8e503e1f530917b6f7b10391d1e4a27efa
Kan215 <katamialvin215@gmail.com>
2023-11-02 14:59:00 -0700

    super

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.c"
+/**
+* _issuper: -checks if a number is super
+* x - is the number to be chaecked
+* Return: 1 for super and 0 otherwise
+*/
+int _issuper(int x)
+{
+if(x >= 65 && x <= 90)
+{
+return (1);
+}
+return (0);
+}
