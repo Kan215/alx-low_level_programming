2dc92073e5ef83d8791f0bb02dd77acf009d3b9c
Kan215 <katamialvin215@gmail.com>
2023-11-02 15:39:06 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print numbers from 0 to 9
+* do not print 2 and 4
+*Return: numbers
+*/
+ void print_numbers(void)
+{
+int a = 0;
+for (; a <= 9; a++)
+{
+if (a == 2 || a ==4)
+{
+continue;
+}
+else
+}
+_putchar(a + '0');
+}
+}
+_putchar('\n');
+}
