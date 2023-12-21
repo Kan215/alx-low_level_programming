c8c7d23d1f21cec9046ccd985ded3eb2e9a5ea82
root <katamialvin215@gmail.com>
2023-12-21 06:14:02 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,22 @@
  */
 void print_triangle(int size)
 {
-    int i, space, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	for (int i = 1; i <= size; i++)
+	{
+		/* Leading spaces */
+		for (int space = size - i; space > 0; space--)
+			_putchar(' ');
 
-    for (i = 1; i <= size; i++)
-    {
-        /* Leading spaces */
-        for (space = size - i; space > 0; space--)
-            _putchar(' ');
+		/* '#' characters */
+		for (int j = 1; j <= i; j++)
+			_putchar('#');
 
-        /* '#' characters */
-        for (j = 1; j <= i; j++)
-            _putchar('#');
-
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
