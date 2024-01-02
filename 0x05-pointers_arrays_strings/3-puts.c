b559352c83c0c7ab20edc8088e609beddac22479
root <katamialvin215@gmail.com>
2024-01-02 11:09:55 -0800

    _puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,13 +4,16 @@
 /**
  * _puts - Prints a string followed by a new line to stdout
  * @str: The string to be printed
+ *
+ * Description: This function prints each character of the given string
+ * followed by a new line.
  */
 void _puts(char *str)
 {
-    while (*str != '\0')
-    {
-        putchar(*str);
-        str++;
-    }
-    putchar('\n');
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
 }
