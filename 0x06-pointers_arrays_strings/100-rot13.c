4e7855e5ad331262077f812d0e8e3dc862dc663f
root <katamialvin215@gmail.com>
2024-01-05 12:19:42 -0800

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * rot13 - Encodes a string using rot13.
- * @s: The string to be encoded.
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
  * Return: *s
  */
+
 char *rot13(char *s)
 {
 	int i;
 	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEEFGHIJKMnopqrstuvwxyzabcdefghijklm";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
