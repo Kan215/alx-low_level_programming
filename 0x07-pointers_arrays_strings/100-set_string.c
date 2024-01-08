fe6ffb9a5426b12a66f23256e2a8f3a4cf4cd3cd
root <katamialvin215@gmail.com>
2024-01-08 13:13:38 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - Entry point
- * @a: input
- * @size: input
+ * se_string - Entry point
+ * @s: input
+ * @to: input
  * Return: Always 0 (Success)
  */
 void set_string(char **s, char *to)
