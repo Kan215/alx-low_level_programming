5d050fcfd64765aac312dea6ad535377df3dd5a0
root <katamialvin215@gmail.com>
2024-01-10 10:42:04 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev__recursion - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: the string to be printed
  * Return: NOthing
  */
