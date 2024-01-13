8da8bf916e7339d81d866e98d43fecd9d57d5e93
root <katamialvin215@gmail.com>
2024-01-12 23:08:53 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - outputs the main
+ * @argc: this is the argument count
+ * @argv: The argument vector
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
